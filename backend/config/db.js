0f2880677e9054d9f31caa10c1f377dcdc35e2b1
Alcarian <floa2@msn.com>
2023-04-04 11:04:58 +0200

    modif server7

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,7 +1,7 @@
 const dotenv = require("dotenv");
 dotenv.config();
 
-const mysql = require("mysql2/promise");
+const mysql = require("mysql2");
 
 const urlDB = `mysql://${process.env.MYSQLUSER}:${process.env.MYSQLPASSWORD}@${process.env.MYSQLHOST}:${process.env.MYSQLPORT}/${process.env.MYSQLDATABASE}`;
 

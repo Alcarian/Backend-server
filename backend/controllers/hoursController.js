85ae45588ca54ec1e1d4031f26dfb50e51e6d467
Alcarian <floa2@msn.com>
2023-04-14 16:00:58 +0200

    controller hours

--- backend/controllers/hoursController.js
+++ backend/controllers/hoursController.js
@@ -24,8 +24,48 @@ exports.updateHours = async (req, res) => {
 
     const [results] = await mysqlConnection.promise().query(querySql, [id]);
 
-    console.log("==> RESULTS");
+    console.log("==> RESULTS <==");
     console.log(results);
+
+    const hoursObject = {
+      open_lunch: req.body.open_lunch,
+      close_lunch: req.body.close_lunch,
+      open_diner: req.body.open_diner,
+      close_diner: req.body.close_diner,
+    };
+
+    console.log("****hoursObject****");
+    console.log(hoursObject);
+
+    const { open_lunch, close_lunch, open_diner, close_diner } = hoursObject;
+
+    const updateSql = `
+      UPDATE hours SET
+      open_lunch = ?,
+      close_lunch = ?,
+      open_diner = ?,
+      close_diner = ?,
+      WHERE id = ?
+    `;
+
+    const valuesUpdate = [open_lunch, close_lunch, open_diner, close_diner, id];
+
+    console.log("==> valuesUpdate <==");
+    console.log(valuesUpdate);
+
+    mysqlConnection
+      .promise()
+      .query(updateSql, valuesUpdate, (error, results) => {
+        if (error) {
+          console.log(error);
+          res.status(500).json({ error });
+        } else {
+          res.status(201).json({
+            message: "Mise à jour des horaires effectuée !",
+            results,
+          });
+        }
+      });
   } catch (error) {
     res.status(500).json({ error });
   }

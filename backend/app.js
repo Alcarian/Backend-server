b6c27d3a4a9b5911674f511c6b2980277cff73d2
Alcarian <floa2@msn.com>
2023-04-17 15:33:17 +0200

    modif cors app 6

--- backend/app.js
+++ backend/app.js
@@ -2,7 +2,7 @@ const express = require("express");
 const morgan = require("morgan");
 const mysql = require("./config/db");
 const http = require("http");
-const cors = require("cors");
+// const cors = require("cors");
 const userRoutes = require("./routes/userRoute");
 const menuRoutes = require("./routes/menuRoute");
 const adminRoutes = require("./routes/adminRoutes");
@@ -12,32 +12,25 @@ const hoursRoutes = require("./routes/hoursRoute");
 // créer une application express
 const app = express();
 
-// Utilisation des cors
-app.use(
-  cors({
-    origin: "http://localhost:3000",
-  })
-);
-
 //importation de body-parser
 const bodyparser = require("body-parser");
 
 // log des req et res
 app.use(morgan("dev"));
 
-//gérer les problèmes de CORS (Cross-Origin-Request-Sharing)
-// app.use((req, res, next) => {
-//   res.setHeader("Access-Control-Allow-Origin", "*");
-//   res.setHeader(
-//     "Access-Control-Allow-Headers",
-//     "Origin, X-Requested-With, Content, Accept, Content-Type, Authorization"
-//   );
-//   res.setHeader(
-//     "Access-Control-Allow-Methods",
-//     "GET, POST, PUT, DELETE, PATCH, OPTIONS"
-//   );
-//   next();
-// });
+// gérer les problèmes de CORS (Cross-Origin-Request-Sharing)
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content, Accept, Content-Type, Authorization"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, PUT, DELETE, PATCH, OPTIONS"
+  );
+  next();
+});
 
 // transformer le body en format json utilisable
 app.use(bodyparser.json());

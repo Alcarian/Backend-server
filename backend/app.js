48c06af5a1afc33eb955b6ebd55c5fecc61ba673
Alcarian <floa2@msn.com>
2023-05-05 13:13:30 +0200

    modif cors app 3

--- backend/app.js
+++ backend/app.js
@@ -29,6 +29,7 @@ app.use((req, res, next) => {
     "Access-Control-Allow-Methods",
     "GET, POST, PUT, DELETE, PATCH, OPTIONS"
   );
+  res.setHeader("Access-Control-Allow-Credentials", "true");
   next();
 });
 

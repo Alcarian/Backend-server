338e7de58024360883c3f0a053df185b1b5a0a40
Alcarian <floa2@msn.com>
2023-06-01 11:36:40 +0200

    app netlify

--- backend/app.js
+++ backend/app.js
@@ -19,6 +19,7 @@ app.use((req, res, next) => {
   const allowedOrigins = [
     "http://localhost:3000",
     "https://backend-server-production.up.railway.app",
+    "https://alcarian-quai-antique.netlify.app/",
   ];
   const origin = req.headers.origin;
   if (allowedOrigins.includes(origin)) {
